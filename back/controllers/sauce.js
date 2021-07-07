42a898459a6543df5ecaa494d7f5519138330704
ColeeneP <coleene.poudevigne@live.fr>
2021-07-07 16:57:08 +0200

    Initialisation du repo, ajout du front, création des fichiers du back'

---
+++ back/controllers/sauce.js
@@ -0,0 +1,75 @@
+const Thing = require('../models/thing');
+const fs = require('fs');
+
+exports.createThing = (req, res, next) => {
+    const thing = new Thing({
+        // id: ,
+        // userId: ,
+        // name: ,
+        // manufacturer: ,
+        // description: ,
+        // mainPepper: ,
+        // imageUrl: ,
+        // heat: ,
+        // likes: ,
+        // dislikes: ,
+        // usersLiked: ,
+        // usersDisliked: 
+    });
+    thing.save().then(
+      () => {
+        res.status(201).json({
+          message: 'Post saved successfully!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  };
+  
+  exports.getOneThing = (req, res, next) => {
+    Thing.findOne({
+      _id: req.params.id
+    }).then(
+      (thing) => {
+        res.status(200).json(thing);
+      }
+    ).catch(
+      (error) => {
+        res.status(404).json({
+          error: error
+        });
+      }
+    );
+  };
+  
+  exports.deleteThing = (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+      .then(thing => {
+        const filename = thing.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Thing.deleteOne({ _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+            .catch(error => res.status(400).json({ error }));
+        });
+      })
+      .catch(error => res.status(500).json({ error }));
+  };
+  
+  exports.getAllStuff = (req, res, next) => {
+    Thing.find().then(
+      (things) => {
+        res.status(200).json(things);
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  };
